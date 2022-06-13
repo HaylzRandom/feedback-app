fb67d4cb6fc26a93affc81b3029838f0c79639c2
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-06-13 18:58:41 +0100

    Added feedback form component, rating select component, button compoment and implemented form validation. Also implemented animation for adding and removing feedback

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import Header from './components/Header';
 
@@ -5,19 +6,27 @@ import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 
 import FeedbackData from './data/FeedbackData';
+import FeedbackForm from './components/FeedbackForm';
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
+
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4();
+		setFeedback([newFeedback, ...feedback]);
+	};
 
 	const deleteFeedback = (id) => {
 		if (window.confirm('Are you sure you want to delete?')) {
 			setFeedback(feedback.filter((item) => item.id !== id));
 		}
 	};
+
 	return (
 		<>
 			<Header />
 			<div className='container'>
+				<FeedbackForm handleAdd={addFeedback} />
 				<FeedbackStats feedback={feedback} />
 				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
 			</div>
