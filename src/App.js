69465e49da8238ee2693299c7e09f5652489e7eb
hayley.mcc@gmail.com <hayley.mcc@gmail.com>
2022-06-16 12:55:18 +0100

    Created routes and links for project linking to homepage and about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
-import Header from './components/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+// Components
+import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
-
-import FeedbackData from './data/FeedbackData';
 import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
+
+// Pages
+import AboutPage from './pages/AboutPage';
+
+// Data
+import FeedbackData from './data/FeedbackData';
 
 function App() {
 	const [feedback, setFeedback] = useState(FeedbackData);
@@ -23,14 +30,29 @@ function App() {
 	};
 
 	return (
-		<>
+		<Router>
 			<Header />
 			<div className='container'>
-				<FeedbackForm handleAdd={addFeedback} />
-				<FeedbackStats feedback={feedback} />
-				<FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+				<Routes>
+					<Route
+						exact
+						path='/'
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedback={feedback} />
+								<FeedbackList
+									feedback={feedback}
+									handleDelete={deleteFeedback}
+								/>
+							</>
+						}></Route>
+
+					<Route path='/about' element={<AboutPage />} />
+				</Routes>
 			</div>
-		</>
+			<AboutIconLink />
+		</Router>
 	);
 }
 
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -7,13 +7,6 @@ function FeedbackList({ feedback, handleDelete }) {
 	if (!feedback || feedback.length === 0) {
 		return <p>No Feedback Yet</p>;
 	}
-	/* return (
-		<div className='feedback-list'>
-			{feedback.map((item) => (
-				<FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-			))}
-		</div>
-	); */
 
 	return (
 		<div className='feedback-list'>
